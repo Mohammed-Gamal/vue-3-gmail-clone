ef24dce2b97e8fa367248e548f2deb0d1c85dc31
Mohamed Gamal <1Mohamed.Gamal54@gmail.com>
2022-09-19 00:24:32 +0200

    Base functionalities & interface

--- src/views/VMailInbox.vue
+++ src/views/VMailInbox.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="home">
-    <h1>VMail Inbox Page</h1>
-  </div>
+  <h1>VMail Inbox</h1>
+
+  <Suspense>
+    <!-- component with async dependency -->
+    <template #default>
+      <MailTable />
+    </template>
+
+    <!-- loading state via #fallback slot -->
+    <template #fallback>
+      Loading...
+    </template>
+  </Suspense>
 </template>
+
+
+<script>
+  import MailTable from '@/components/MailTable.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      MailTable
+    }
+  }
+</script>
